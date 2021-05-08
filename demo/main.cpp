a3e6a9a74f1e8e53529df4ff36f39503a6d76ba3
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2021-05-09 02:00:56 +0300

    demo main

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,18 +1,34 @@
 #include "../src/fs_shell.hpp"
 
-//#include <streambuf>
-//#include <iostream>
+#include <streambuf>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 
 int main() {
-//    std::stringstream ss{};
-//    std::iostream ios{ss.rdbuf()};
-//
-//    ios << "in 1 1 32 32 a.fs\n";
-//    ios << "sv\n";
-//    ios << "exit\n";
-//
-//    shell::run(ios, true);
+    std::string str;
+    while (str != "exit") {
+        std::cout << "Name of file with scenario:";
+        std::cin >> str;
+        if (str == "exit") {
+            break;
+        }
+
+        str = std::string("./scripts/").append(str);
+
+        std::ifstream ifs{str, std::ios::in};
+        if (!ifs.is_open()) {
+            ifs = std::ifstream{str.append(".txt"), std::ios::in};
+
+            if (!ifs.is_open()) {
+                std::cout << "File with provided name does not exist\n\n";
+                continue;
+            }
+        }
+        shell::run(ifs, true);
+        std::cout << std::endl;
+    }
 
     return 0;
 }
